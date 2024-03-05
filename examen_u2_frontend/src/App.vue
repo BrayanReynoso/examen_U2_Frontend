b0d565bc3d286184290c49d2327dfb7b04d7a5bb
Brayan Reynoso <20213tn084@utez.edu.mx>
2024-03-04 19:00:43 -0600

    [ADD] botones de filtro

--- examen_u2_frontend/src/App.vue
+++ examen_u2_frontend/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
 import Carousel from './components/CarouselComponent.vue';
+const handleClick = (message) => {
+  console.log(message);
+}
 </script>
 
 <template>
-  <div id="app">
-   <Carousel/>
+  <div id="container" class="text-center">
+    <Carousel />
+    <div class="mt-4">
+      <b-button @click="handleClick('Ordenar por autor')" class="me-2">Ordenar por autor</b-button>
+      <b-button @click="handleClick('Ordenar por fecha')" class="me-2">Ordenar por fecha</b-button>
+      <b-button @click="handleClick('Mostrar si tiene imagen')" >Mostrar si tiene imagen</b-button>
+    </div>
   </div>
 </template>
